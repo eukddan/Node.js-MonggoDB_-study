358445f6f6f05e85951c1509f8c1bea05b87bba3
김유찬 <dkejdlcl123@naver.com>
2023-09-25 18:07:50 +0900

    SendHtmlFile(2)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행 중");
 });
@@ -11,8 +13,4 @@ app.get("/", (요청, 응답) => {
 
 app.get("/news", (요청, 응답) => {
   응답.send("오늘 비옴");
-});
-
-app.get("/about", (요청, 응답) => {
-  응답.sendFile(__dirname + "/introduce.html");
 });

0643c3b0bfabb67ce7aab7e73ac3ad4288362bb5
김유찬 <dkejdlcl123@naver.com>
2023-10-08 22:58:16 +0900

    AWS S3 환경설정

--- server.js
+++ server.js
@@ -33,6 +33,27 @@ app.use(
 
 app.use(passport.session());
 
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "eukddan",
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString()); //업로드시 파일명 변경가능
+    },
+  }),
+});
+
 let db;
 const url = process.env.DB_URL;
 new MongoClient(url)
